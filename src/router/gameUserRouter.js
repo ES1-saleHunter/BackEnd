4fd30013d22530c70a8768af897486d4e50a29fc
thisisrenan <joserenandasilva08@gmail.com>
2023-05-27 15:24:03 -0300

    historia 18 backend concluido

--- src/router/gameUserRouter.js
+++ src/router/gameUserRouter.js
@@ -5,6 +5,7 @@ const relationMiddleware = require("../middleware/relationGameUserMiddleware");
 const jwt_verification = require("../middleware/jwt");
 
 router.post("/relationgametouser",jwt_verification.jwt_verification,gameuserController.relationships_game_user , relationMiddleware.add_valor_relation);
+router.post("/removerelationgametouser",jwt_verification.jwt_verification, gameuserController.remove_relationships_game_user);
 router.get("/getusergames", jwt_verification.jwt_verification, gameuserController.get_user_game);
 
 module.exports = router;
