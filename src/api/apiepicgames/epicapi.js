55bc5f005e3fdf129070602fc6c8f8a9b0e9ae59
unknown <joserenandasilva08@gmail.com>
2023-06-01 12:15:24 -0300

    arrumando erro no fetch

--- src/api/apiepicgames/epicapi.js
+++ src/api/apiepicgames/epicapi.js
@@ -1,5 +1,4 @@
 
-const fetch = require("node-fetch");
 const axios = require('axios');
 
 const pathlinkpromocao = "https://store.steampowered.com/"
@@ -13,7 +12,7 @@ const getprice = async (name) => {
 
 const getpromotion = async () => {
     let variaveis = `https://store.epicgames.com/graphql?operationName=searchStoreQuery&variables=%7B%22allowCountries%22:%22BR%22,%22category%22:%22games%2Fedition%2Fbase%22,%22count%22:40,%22country%22:%22BR%22,%22effectiveDate%22:%22[,2023-05-27T23:44:01.631Z]%22,%22keywords%22:%22%22,%22locale%22:%22pt-BR%22,%22onSale%22:true,%22sortBy%22:%22relevancy,viewableDate%22,%22sortDir%22:%22DESC,DESC%22,%22start%22:0,%22tag%22:%22%22,%22withPrice%22:true%7D&extensions=%7B%22persistedQuery%22:%7B%22version%22:1,%22sha256Hash%22:%227d58e12d9dd8cb14c84a3ff18d360bf9f0caa96bf218f2c5fda68ba88d68a437%22%7D%7D`
-    let data = await fetch(variaveis, {
+    const data = await fetch(variaveis, {
         "headers": {
           "accept": "application/json, text/plain, */*",
           "accept-language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
