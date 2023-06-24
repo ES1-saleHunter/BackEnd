46d61b985b7a4c1ea440ca6deaf4227bb8d96d0a
thisisrenan <joserenandasilva08@gmail.com>
2023-06-24 17:48:04 -0300

    História 28 completa

--- src/router/gameUserRouter.js
+++ src/router/gameUserRouter.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const gameuserController = require("../controller/gameUserController");
+const gameuserLikeController = require("../controller/gameUserLikeController");
 const relationMiddleware = require("../middleware/relationGameUserMiddleware");
 const jwt_verification = require("../middleware/jwt");
+const likesControle = require("../controller/gameController")
 
 router.post("/relationgametouser",jwt_verification.jwt_verification,gameuserController.relationships_game_user , relationMiddleware.add_valor_relation);
 router.post("/removerelationgametouser",jwt_verification.jwt_verification, gameuserController.remove_relationships_game_user);
 router.get("/getusergames", jwt_verification.jwt_verification, gameuserController.get_user_game);
+
+router.post("/relationgametouserlike",jwt_verification.jwt_verification,gameuserLikeController.relationships_game_user, likesControle.update_game_likes);
+router.post("/removerelationgametouserlike",jwt_verification.jwt_verification, gameuserLikeController.remove_relationships_game_user, likesControle.remove_game_likes);
+router.get("/getusergameslike", jwt_verification.jwt_verification, gameuserLikeController.get_user_game);
 
 module.exports = router;
